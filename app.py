d12944e9d16a4729841e859e62c786100693a115
aradmjn96-source <aradmjn96@gmail.com>
2026-09-16 22:18:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(
-        app="Yobi1",
-        version="0.1.0",
-        status="Yobi1 Server is running!"
-    )
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
-    return jsonify(ok=True)
+    return {"ok": True}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
